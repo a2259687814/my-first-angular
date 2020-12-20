bfcbf007dd7ffa5eb244b10b0d6f19aad187d2f1
Vasile Home <chisinau2000>
2020-12-20 19:53:57 +0200

    primul capitol baza

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -9,4 +9,6 @@ import {Component} from "@angular/core";
 export class PostComponent {
   title = 'Dynamic tytle'
 
+  now:Date = new Date();
+
 }
--- src/app/post2/post2.component.ts
+++ src/app/post2/post2.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Post2Component implements OnInit {
 
+  backgroundTouggle = false
+  arr = [1, 1, 2, 3, 5, 8]
+
+  objs = [
+    {title: 'Post 1', author:'Vasile', coments:[
+        {name: 'Max', text:'lorem 1'},
+        {name: 'Max', text:'lorem 2'},
+        {name: 'Max', text:'lorem 3'},
+      ]},
+    {title: 'Post 2', author:'Vasile2', coments:[
+        {name: 'Max 2', text:'lorem 1'},
+        {name: 'Max 2', text:'lorem 2'},
+        {name: 'Max 2', text:'lorem 3'},
+      ]},
+  ]
   constructor() { }
 
   ngOnInit(): void {
