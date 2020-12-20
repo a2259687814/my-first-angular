e2f392010377afcc57a2c6d9e6fcc822efd79d75
Vasile Home <chisinau2000>
2020-12-20 15:58:34 +0200

    first lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-first-angular';
+  title = 'Dynamic tytle2'
+  img = 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg'
+
+  inputValue = ''
+  vasea = 'VASEA'
+  petru='ion'
+
+
+  constructor() {
+    setTimeout(() => {
+      this.img = 'http://frontendlabepam.github.io/FL5/angular/assets/img/angular-js-logo.png'
+    }, 3000)
+  }
+
+  onInput(e: Event) {
+    this.inputValue = (<HTMLInputElement>e.target).value;
+  }
+
+  onBlur(value: string) {
+    this.inputValue = value;
+  }
+
+  onVasea(e: any) {
+    this.vasea = e.target.value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {PostComponent} from './post/post.component';
+import {Post2Component} from './post2/post2.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    Post2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
