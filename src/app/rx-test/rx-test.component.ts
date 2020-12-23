a297c8efe8790ece3d399a44c41b6c17d618fa9a
Vasile Moraru <vasile.moraru@test.test>
2020-12-23 14:41:47 +0200

    Subject

--- src/app/rx-test/rx-test.component.ts
+++ src/app/rx-test/rx-test.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable, Subscription} from 'rxjs';
+import {Subject, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-rx-test',
@@ -9,36 +9,13 @@ import {Observable, Subscription} from 'rxjs';
 export class RxTestComponent implements OnInit {
 
   sub: Subscription;
+  stream$: Subject<number> = new Subject<number>();
+  counter = 0;
 
   constructor() {
-    const stream$ = new Observable(observer => {
-
-      setTimeout(() => {
-        observer.next(1);
-      }, 1500);
-
-
-      setTimeout(() => {
-        observer.complete();
-      }, 2100);
-
-      setTimeout(() => {
-        observer.error('Something went wrong');
-      }, 2000);
-
-
-      setTimeout(() => {
-        observer.next(2);
-      }, 2500);
-
+    this.sub = this.stream$.subscribe(value => {
+      console.log('Subscribe', value);
     });
-
-    this.sub = stream$
-      .subscribe(
-        value => console.log('Next: ', value),
-        error => console.log('Error: ', error),
-        () => console.log('Complete')
-      );
   }
 
   ngOnInit(): void {
@@ -46,5 +23,10 @@ export class RxTestComponent implements OnInit {
 
   stop(): void {
     this.sub.unsubscribe();
+  }
+
+  next() {
+    this.counter++;
+    this.stream$.next(this.counter);
   }
 }
