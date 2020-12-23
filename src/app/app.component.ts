5f9b4efb2b76cb617ee2e77a098aad1c731fc762
Vasile Moraru <vasile.moraru@test.test>
2020-12-23 16:50:37 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+export interface Post {
+  title: string;
+  text: string;
+  id?: number;
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-first-angular';
+  posts: Array<Post> = [
+    {
+      title: 'vasea1',
+      text: 'lorem10 lorem10',
+      id: 1
+    },
+    {
+      title: 'vasea2',
+      text: 'lorem22 lorem22',
+      id: 2
+    }
+  ];
+
+  updatePosts(post: Post): void {
+    this.posts.unshift(post);
+    console.log(post);
+
+  }
 }
