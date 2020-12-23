23f3ece578b077892add339d855e4a25a79e2bcd
Vasile Moraru <vasile.moraru@test.test>
2020-12-23 11:49:41 +0200

    Ad rx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RxTestComponent } from './rx-test/rx-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RxTestComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/rx-test/rx-test.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rx-test',
+  templateUrl: './rx-test.component.html',
+  styleUrls: ['./rx-test.component.scss']
+})
+export class RxTestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
