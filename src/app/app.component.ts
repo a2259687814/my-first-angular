f3f70f166cf860a05a32cf3373349e9ad9b1aeaa
Vasile Home <chisinau2000>
2020-12-26 13:44:58 +0200

    3 capitol

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  isVisible = true;
+
   title = 'my-first-angular';
 }
--- src/app/directives/if-not.directive.ts
+++ src/app/directives/if-not.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import {Directive, Input, TemplateRef, ViewContainerRef} from '@angular/core';
 
 @Directive({
   selector: '[appIfNot]'
 })
 export class IfNotDirective {
 
-  constructor() { }
+  @Input() set ifNot(condition: boolean) {
+    if (!condition) {
+      // show
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    } else {
+      // hide
+      this.viewContainer.clear();
+    }
+  }
+
+  constructor(private templateRef: TemplateRef<any>, private viewContainer: ViewContainerRef) {
+
+  }
 
 }
