63736d1c9d92b8df6a79a8425b6ee35669108a0f
vasile.moraru <chisinau2000@gmail.com>
2021-01-07 18:01:19 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,3 +22,4 @@ import {FormsModule} from "@angular/forms";
 export class AppModule { }
 
 // https://stackoverflow.com/questions/12710905/how-do-i-dynamically-assign-properties-to-an-object-in-typescript
+// https://www.youtube.com/watch?v=jgWnccjXR4I
