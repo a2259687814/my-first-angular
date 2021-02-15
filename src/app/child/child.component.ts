734cd0f5728f1879803e7ae797f7ac9e851b3655
vasile.moraru <chisinau2000@gmail.com>
2021-02-15 16:51:39 +0200

    ngZone

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,39 +1,57 @@
-import {ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, NgZone, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, EventEmitter, Input, NgZone, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss'],
-  //changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChildComponent implements OnInit {
   @Input() arr: number[] = [];
+
+  @Output() onAdd: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+
   title = 0;
 
   constructor(private cf: ChangeDetectorRef, private zone: NgZone) {
     // this.cf.detach();
   }
 
+  ngOnInit(): void {
+    setInterval(() => {
+      this.title = Math.random();
+      this.onAdd.emit(true);
+      // this.cf.markForCheck();
+      //this.cf.detectChanges();
+    }, 21000);
+  }
+
   // ngOnInit(): void {
-  //   setInterval(() => {
-  //     this.title = Math.random();
-  //     //this.cf.markForCheck();
-  //     //this.cf.detectChanges();
-  //   }, 3000);
+  //   this.zone.runOutsideAngular(() => {
+  //     setInterval(() => {
+  //       this.title = Math.random();
+  //
+  //     }, 1000);
+  //   });
   // }
 
-  ngOnInit(): void {
-    this.zone.runOutsideAngular(() => {
-      setInterval(() => {
-        this.title = Math.random();
-      }, 1000);
-    });
-
-  }
+  // ngOnInit(): void {
+  //   this.cf.detach();
+  //   this.zone.run(() => {
+  //     setInterval(() => {
+  //       this.title = Math.random();
+  //     }, 1000);
+  //   });
+  // }
 
 
   child(): string {
     console.log('Child');
     return 'Child';
+  }
+
+  vasea(): void {
+    // this.onAdd.emit(true);
   }
 }
