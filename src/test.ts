7ea0dc3f358cacea6ddc411ace9aed4409938e1a
Vasile Moraru <vasile.moraru@amdaris.com>
2021-07-03 14:00:10 +0300

    qqqq

--- src/test.ts
+++ src/test.ts
@@ -11,7 +11,7 @@ declare const require: {
   context(path: string, deep?: boolean, filter?: RegExp): {
     keys(): string[];
     <T>(id: string): T;
-  };
+  };casdas
 };
 
 // First, initialize the Angular testing environment.
